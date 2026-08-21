8293d4d34e7597176da19113b73e60dc29cde256
chetan Paudel <paudelchetan1112@gmail.com>
2026-08-21 22:15:11 +0545

    server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
-func main(){
-fmt.Println("olx-api running....")
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"Status":"Ok"}`))
+	})
+	srv := http.Server{
+		Addr:         ":8090",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed:%v", err)
+	}
+
+	fmt.Println("Server is running..")
 
 }
